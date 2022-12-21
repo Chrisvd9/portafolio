6b5443542b6268baa13a876941669c88ebaccc77
Chrisvd9 <christianvd99@gmail.com>
2022-12-21 16:29:54 -0300

    fix bug2

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,8 +14,8 @@ const Nav = () => {
         <nav>
             <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
             <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
-            <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
-            <a href="#portafolio" onClick={()=> setActiveNav('#portafolio')} className={activeNav === '#portafolio' ? 'active' : ''}><RiServiceLine/></a>
+            <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><RiServiceLine/></a>
+            <a href="#portafolio" onClick={()=> setActiveNav('#portafolio')} className={activeNav === '#portafolio' ? 'active' : ''}><BiBook/></a>
             <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
         </nav>
     );
