982f9897e385bb5b9a79c7e811d548b4547de5c9
Chrisvd9 <christianvd99@gmail.com>
2023-01-12 14:38:55 -0300

    actualización 2023

--- src/components/portafolio/Portafolio.jsx
+++ src/components/portafolio/Portafolio.jsx
@@ -1,88 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { data } from "../../data";
 import "./portafolio.css";
-import IMG1 from "../../assests/homepage.png";
-import IMG2 from "../../assests/homepageferia.png";
-import IMG3 from "../../assests/blackjacksc.png";
-import IMG4 from "../../assests/quizapp.png";
-import IMG5 from "../../assests/shorturl.png";
-import IMG6 from "../../assests/ad-1.png";
-import IMG7 from "../../assests/app-planificador.png";
-import IMG8 from "../../assests/inicio-criptos.png";
-import IMG9 from "../../assests/home-rootlab.png";
-import IMG10 from "../../assests/home-docmovi.png";
-
-const data = [
-  {
-    id: 1,
-    image: IMG1,
-    title: "Almond Web Project",
-    github: "https://github.com/Chrisvd9/Almond-Web-Project",
-    demo: "http://34.234.77.210/",
-  },
-  {
-    id: 2,
-    image: IMG2,
-    title: "Feria Virtual Project",
-    github: "https://github.com/Chrisvd9/feria-virtual-projectv2",
-    demo: "",
-  },
-  {
-    id: 3,
-    image: IMG3,
-    title: "BlackJack",
-    github: "https://github.com/Chrisvd9/simple-blackjack-game",
-    demo: "https://chrisvd9.github.io/simple-blackjack-game/",
-  },
-  {
-    id: 4,
-    image: IMG4,
-    title: "Quizapp",
-    github: "https://github.com/Chrisvd9/quizapp",
-    demo: "https://quizappchris.herokuapp.com/",
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: "Ushort",
-    github: "https://github.com/Chrisvd9/ShortUrls",
-    demo: "https://shorturls-production-fe26.up.railway.app/",
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: "Administrador de pacientes veterinaria",
-    github: "https://github.com/Chrisvd9/Administrador-de-pacientes-vet",
-    demo: "https://clinquant-granita-a7b6f4.netlify.app/",
-  },
-  {
-    id: 7,
-    image: IMG7,
-    title: "Administrador de gastos",
-    github: "https://github.com/Chrisvd9/Administrador-de-gastos",
-    demo: "https://bespoke-tulumba-ed6bd5.netlify.app/",
-  },
-  {
-    id: 8,
-    image: IMG8,
-    title: "Cotizador de criptomonedas",
-    github: "https://github.com/Chrisvd9/Cotizador-de-criptomonedas",
-    demo: "https://frolicking-pastelito-9bf01d.netlify.app/",
-  },
-  {
-    id: 9,
-    image: IMG9,
-    title: "Productos rootlab nextjs",
-    github: "https://github.com/Chrisvd9/productos-rootlab-nextjs-ts",
-    demo: "https://productos-rootlab-nextjs-ts-by-christian-vergara.vercel.app/",
-  },
-  {
-    id: 10,
-    image: IMG10,
-    title: "Docmovi test 2022",
-    github: "https://github.com/Chrisvd9/docmovi-test-2022",
-    demo: "",
-  },
-];
 
 const Portafolio = () => {
   return (
@@ -93,18 +12,27 @@ const Portafolio = () => {
       <div className="container portafolio__container">
         {data?.map(({ id, image, title, github, demo }) => {
           return (
-            <article className="portafolio__item">
+            <article key={id} className="portafolio__item">
               <div className="portafolio__item-image">
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
               <div className="portafolio__item-cta">
-                <a href={github} className="btn" target="_blank">
+                <a href={github} className="btn" target="_blank" rel="noopener">
                   GitHub
                 </a>
-                <a href={demo} className="btn btn-primary" target="_blank">
-                  Live Demo
-                </a>
+                {demo ? (
+                  <a
+                    href={demo}
+                    className="btn btn-primary"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    Live Demo
+                  </a>
+                ) : (
+                  ""
+                )}
               </div>
             </article>
           );
